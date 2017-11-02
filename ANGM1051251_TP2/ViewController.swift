80e412617a65024809047777db478ddca511f6b7
Maria Angulo <1051251@cstj.qc.ca>
2017-11-02 13:20:12 -0400

    Initial Commit

---
+++ ANGM1051251_TP2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ANGM1051251_TP2
+//
+//  Created by Maria Angulo on 17-11-02.
+//  Copyright © 2017 Maria Angulo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
